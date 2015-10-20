b4b7c3513abaedc679389b84b65d511e2a52c261
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-10-20 16:31:38 +0800

    add player

--- server/main.js
+++ server/main.js
@@ -1,8 +1,35 @@
 (function(){
-    var url = "video_repo/SampleVideo_1080x720_5mb/SampleVideo_1080x720_5mb.mpd";
-    var context = new Dash.di.DashContext();
-    var player = new MediaPlayer(context);
-    player.startup();
-    player.attachView(document.querySelector("#videoPlayer"));
-    player.attachSource(url);
+    
+    var app = angular.module("app", []);
+    app.controller("AppController", ["$scope", "$http", AppController]);
+    
+    function AppController($scope, $http) {
+    	$scope.currentVideo = "SampleVideo_1080x720_5mb.mpd";
+    	$scope.videos = [];
+    	$scope.playVideo = playVideo;
+
+    	$http.get('/cs5248/mpdList.php')
+    	.then(function(res) {
+    		for (var i = res.data.length - 1; i >= 0; i--) {
+    			$scope.videos.push({name: getNameFromUrl(res.data[i]), url: res.data[i]});
+    		};
+    	}, 
+    	function() {
+    		$scope.videos = [];
+    	});
+
+    	function playVideo(url) {
+		    var context = new Dash.di.DashContext();
+		    var player = new MediaPlayer(context);
+		    player.startup();
+		    player.attachView(document.querySelector("#videoPlayer"));
+		    player.attachSource(url);	
+    	}
+
+    	function getNameFromUrl(url) {
+    		var startNamePos = url.lastIndexOf("/");
+    		var name = url.substring(startNamePos + 1);
+    		return name;
+    	}
+    }
 })();
