648e1f148e207540c3bcecc62d61ab809c92aa7f
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-10-27 23:11:13 +0800

    fix path problem

--- server/dash.php
+++ server/dash.php
@@ -184,14 +184,97 @@ function makeHls($sourceVideoPath) {
 	$fps = $videoInfo["video"]["frame_rate"];
 	$tentativeSegmentDuration = 10; //in seconds
 
-	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 3072 ".$fps." ".$width."x".$height." 44100 128 ".$videoResultPath.".ts");
-	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 3072 ".$fps." ".$width."x".$height." 44100 128 ".$videoResultPath.".mp4");
+	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 3072 ".$fps." ".$width."x".$height." 44100 128 ".$videoResultPath.DIRECTORY_SEPARATOR.$videoName."_original.ts");
+	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 3072 ".$fps." ".$width."x".$height." 44100 128 ".$videoResultPath.DIRECTORY_SEPARATOR.$videoName."_original.mp4");
 
 
-	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 768 ".$fps." ".($width / 2)."x".($height / 2)." 44100 128 ".$videoResultPath."_half.ts");
-	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 768 ".$fps." ".($width / 2)."x".($height / 2)." 44100 128 ".$videoResultPath."_half.mp4");
-	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 200 ".$fps." ".($width / 4)."x".($height / 4)." 44100 128 ".$videoResultPath."_quarter.ts");
-	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 200 ".$fps." ".($width / 4)."x".($height / 4)." 44100 128 ".$videoResultPath."_quarter.mp4");
+	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 768 ".$fps." ".($width / 2)."x".($height / 2)." 44100 128 ".$videoResultPath.DIRECTORY_SEPARATOR.$videoName."_half.ts");
+	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 768 ".$fps." ".($width / 2)."x".($height / 2)." 44100 128 ".$videoResultPath.DIRECTORY_SEPARATOR.$videoName."_half.mp4");
+	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 200 ".$fps." ".($width / 4)."x".($height / 4)." 44100 128 ".$videoResultPath.DIRECTORY_SEPARATOR.$videoName."_quarter.ts");
+	echo shell_exec("/usr/local/bin/convert.sh ".$sourceVideoPath." 200 ".$fps." ".($width / 4)."x".($height / 4)." 44100 128 ".$videoResultPath.DIRECTORY_SEPARATOR.$videoName."_quarter.mp4");
+
+	//make mpd file for videos
+	//ORIGINAL
+	$mpdPath = $videoResultPath . DIRECTORY_SEPARATOR . $videoName;
+	$command = 'MP4Box'
+				. ' -out ' . $mpdPath
+				. ' -dash 10000 -frag 10000 -rap'
+				. ' -segment-name ' . $videoName . '_original_ '
+				. $videoResultPath . DIRECTORY_SEPARATOR . $videoName . "_original.mp4#video";
+	shell_exec($command);
+
+	//HALF
+	$halfMpdPath = $videoResultPath . DIRECTORY_SEPARATOR . $videoName . "_half";
+	$command = 'MP4Box'
+				. ' -out ' . $halfMpdPath
+				. ' -dash 10000 -frag 10000 -rap'
+				. ' -segment-name ' . $videoName . '_half_ '
+				. $videoResultPath . DIRECTORY_SEPARATOR . $videoName . "_half.mp4#video";
+	shell_exec($command);
+
+	//QUARTER
+	$quarterMpdPath = $videoResultPath . DIRECTORY_SEPARATOR . $videoName . "_quater";
+	$command = 'MP4Box'
+				. ' -out ' . $quarterMpdPath
+				. ' -dash 10000 -frag 10000 -rap'
+				. ' -segment-name ' . $videoName . '_quarter_ '
+				. $videoResultPath . DIRECTORY_SEPARATOR . $videoName . "_quarter.mp4#video";
+	shell_exec($command);
+
+	//make mpd file for audio
+	$audioMpdPath = $videoResultPath . DIRECTORY_SEPARATOR . $videoName . "_audio";
+	$command = 'MP4Box'
+				. ' -out ' . $audioMpdPath
+				. ' -dash 10000 -frag 10000 -rap'
+				. ' -segment-name ' . $videoName . '_audio_ '
+				. $sourceVideoPath . "#audio";
+	shell_exec($command);
+
+	// //merge mpd files
+	$mpd = file_get_contents($mpdPath . ".mpd");
+	$mpdHalf = file_get_contents($halfMpdPath . ".mpd");
+	$mpdQuarter = file_get_contents($quarterMpdPath . ".mpd");
+	$mpdAudio = file_get_contents($audioMpdPath . ".mpd");
+
+	//video half
+	$adaptationSetEndPos = strrpos($mpd, "</AdaptationSet>");
+	if($adaptationSetEndPos !== false) {
+		$startCut = strrpos($mpdHalf, "<Representation");
+		$endCut = strrpos($mpdHalf, "</AdaptationSet>");
+		$representationContent = substr($mpdHalf, $startCut, $endCut - $startCut);
+		$mpd = substr_replace($mpd, $representationContent, $adaptationSetEndPos, 0);
+	}
+	//video quarter
+	$adaptationSetEndPos = strrpos($mpd, "</AdaptationSet>");
+	if($adaptationSetEndPos !== false) {
+		$startCut = strrpos($mpdQuarter, "<Representation");
+		$endCut = strrpos($mpdQuarter, "</AdaptationSet>");
+		$representationContent = substr($mpdQuarter, $startCut, $endCut - $startCut);
+		$mpd = substr_replace($mpd, $representationContent, $adaptationSetEndPos, 0);
+	}
+
+	//audio
+	$periodEndPos = strrpos($mpd, "</Period>");
+	if($periodEndPos !== false) {
+		$startCut = strrpos($mpdAudio, "<AdaptationSet");
+		$endCut = strrpos($mpdAudio, "</Period>");
+		$representationContent = substr($mpdAudio, $startCut, $endCut - $startCut);
+		$mpd = substr_replace($mpd, $representationContent, $periodEndPos, 0);
+	}
+
+	//fix bug of path not found
+	$mpd = str_replace($videoRepoPath . "/" . $videoName . "/", "", $mpd);
+
+	//remove other mpd file
+	$files = glob($videoResultPath . DIRECTORY_SEPARATOR . '*.mpd'); // get all file names
+	foreach($files as $file){
+	  if(is_file($file))
+	    unlink($file); // delete file
+	}
+
+	$mpdFile = fopen($mpdPath . ".mpd", "w");
+	fwrite($mpdFile, $mpd);
+	fclose($mpdFile);
 }
 
 ?>
